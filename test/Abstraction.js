9568ddf1d51ec3449d78cb0b11a4e571b029bef0
unknown <mrraymoon1@gmail.com>
2023-11-19 23:56:36 +0100

    Update tests

--- test/Abstraction.js
+++ test/Abstraction.js
@@ -3,11 +3,18 @@ const { ethers } = require("hardhat");
 
 describe("Abstraction", function () {
   it("Should Mint the right quantity of NFT", async function () {
-    const Abstraction = await ethers.getContractFactory("Greeter");
-    const abstraction = await Abstraction.deploy("Hello, world!");
+    const Abstraction = await ethers.getContractFactory("Abstraction");
+    const abstraction = await Abstraction.deploy("");
     await abstraction.deployed();
 
     expect(await abstraction.mint()).to.equal("1");
 
   });
+
+  it ("Should transfer the right amount to the NFT buyer", async function() {
+    const AbsContract = await ethers.getContractFactory("Abstraction")
+    const contract = await AbsContract.deploy()
+    await contract.deployed()
+
+  })
 });
